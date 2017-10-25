af346f618663c326da154db994cbc3b5f2cbdbe5
Seth T <seth10@users.noreply.github.com>
2017-10-25 17:16:05 -0400

    Clean up stuff
    Also don't hardcode Coin in the __import__ step (on now line 17)

--- DynamicClassLoading.py
+++ DynamicClassLoading.py
@@ -1,34 +1,21 @@
+# -*- coding: utf-8 -*-
+
 #from Coin import Coin
-#we don't know the name of the classes in advance, so we can't use this
-#btw, why the from Coin part? why not just import coin?
-
-#"Direct use of __import__() is rare, except in cases where you want to import a module whose name is only known at runtime."
-#https://docs.python.org/2/library/functions.html#__import__
-#perfect!
-
-#__import__(name[, globals[, locals[, fromlist[, level]]]])
-
-#https://www.python.org/dev/peps/pep-0008
-
+# we don't know the name of the classes in advance, so we can't use this
+# instead,
 #Coin = __import__("Coin", globals(), locals(), ["Coin"], -1).Coin
 
-#https://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
-#from os import walk
-#for filenames in walk(path):
+import os
 
-from os import listdir
-from os.path import isfile, join
+plugin = {}
 
-plugin = dict()
+for fileName in os.listdir(os.path.dirname(os.path.realpath(__file__))):
+    print fileName
+    if fileName.endswith('.py') and fileName != __file__:
+        p = fileName[:fileName.index('.')]
+        print '└─importing ' + p
+        exec('plugin[p] = __import__(p, globals(), locals(), [p], -1).'+p)
+print '\nDone importing!\n'
 
-path = '/Users/setht/Desktop/Development/g3'
-for file in listdir(path):
-    print file
-    #neither __file__ nor sys.argv[0] work so hardcoding it 4naw
-    if isfile(join(path,file)) and file.endswith(".py") and file != "DynamicClassLoading.py": #is not will not work
-        print "  importing"
-        #file[:-3] lol face
-        plugin[file[:-3]] = __import__(file[:-3], globals(), locals(), [file[:-3]], -1).Coin
-
-coin = plugin["Coin"](1)
+coin = plugin['Coin'](1)
 print coin.value
